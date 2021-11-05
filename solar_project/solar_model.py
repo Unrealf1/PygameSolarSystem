127942063f07db8445008a3b1494b8a5dc5d34c6
Daniel <bdfaraon@gmail.com>
2021-11-05 18:57:19 +0300

    First two tasks assigned to B student are done, model is stable, file is read correctly

--- solar_project/solar_model.py
+++ solar_project/solar_model.py
@@ -20,9 +20,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R + obj.R) # обработка аномалий при прохождении одного тела сквозь другое
-        body.Fx += -gravitational_constant*obj.m*body.m*(body.x - obj.x)/r**3
-        body.Fy += -gravitational_constant*obj.m*body.m*(body.y - obj.y)/r**3
+        r = max(r, body.R + obj.R)  # обработка аномалий при прохождении одного тела сквозь другое
+        body.Fx += -gravitational_constant * obj.m * body.m * (body.x - obj.x) / r ** 3
+        body.Fy += -gravitational_constant * obj.m * body.m * (body.y - obj.y) / r ** 3
 
 
 def move_space_object(body, dt):
@@ -32,13 +32,12 @@ def move_space_object(body, dt):
 
     **body** — тело, которое нужно переместить.
     """
-    old = body.x
-    ax = body.Fx/body.m
-    body.x += body.Vx*dt + ax*dt**2/2
-    body.Vx += ax*dt
-    ay = body.Fy/body.m
-    body.y += body.Vy*dt + ay*dt**2/2
-    body.Vy += ay*dt
+    ax = body.Fx / body.m
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
+    ay = body.Fy / body.m
+    body.Vy += ay * dt
+    body.y += body.Vy * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
